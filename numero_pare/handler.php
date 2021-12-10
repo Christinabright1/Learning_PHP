3c2efac27c57d45a9f4bd987f13892f2844d661e
Christine Anani <ananichristine2333@gmail.com>
2021-12-10 00:08:37 +0000

    Update handler.php

--- numero_pare/handler.php
+++ numero_pare/handler.php
@@ -1,4 +1,3 @@
-
 <?php
 $n1= $_POST["n1"];
 $n2= $_POST["n2"];
@@ -14,7 +13,26 @@ $n10= $_POST["n10"];
 //declare an empty array
 
 $list_num = array();
+$score =0;
 //push element into array
-array_push($list_num, $n2, $n3,$n4, $n5,  $n6,  $n7,  $n7,  $n8 , $n9, $n10);
-echo ($list_num);
+var_dump(array_push($list_num,$n2,$n3,$n4,$n5,$n6,$n7,$n7,$n8,$n9,$n10));
+print_r($list_num);
+for($x=0; $x<count($list_num) ; $x++){
+    if ($list_num[$x] %2 == 0){
+          $score += $list_num[$x];
+    }else{
+        $score -=$list_num[$x];
+    }
+}
+if($score < -5){
+   echo "péssima pontuação"; 
+}elseif( $score < 0){
+   echo "má pontuaçã";
+}elseif ($score < 5){
+ echo "“pontuação razoável";
+}else{
+    echo "boa pontuação";
+}
+  
+echo $score;
 ?>
